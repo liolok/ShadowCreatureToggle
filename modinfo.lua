b22b13093ff08797d17aee6e1ea01320de998a89
liolok <i@liolok.com>
2025-04-13 00:26:31 +0800

    stylua

--- modinfo.lua
+++ modinfo.lua
@@ -1,16 +1,16 @@
-name = "Bone Helm Shadow Toggle"
+name = 'Bone Helm Shadow Toggle'
 --The name of your mod
-description = "Allows you to turn off shadows once you equip the bone helm"
+description = 'Allows you to turn off shadows once you equip the bone helm'
 --The description that shows when you are selecting the mod from the list
-author = "splorange"
+author = 'splorange'
 --Your name!
-version = "1.1"
+version = '1.1'
 
-forumthread = ""
+forumthread = ''
 
-icon_atlas = "modicon.xml"
+icon_atlas = 'modicon.xml'
 
-icon = "modicon.tex"
+icon = 'modicon.tex'
 
 dst_compatible = true
 forge_compatible = true
@@ -28,102 +28,99 @@ api_version = 10
 --This is the version of the game's API and should stay the same for the most part
 
 local Keys = {
-	{description = "None --", data = -1}, -- If players can't find the None option
-	{description = "Numpad Zero", data = 256},
-	{description = "Numpad One", data = 257},
-	{description = "Numpad Two", data = 258},
-	{description = "Numpad Three", data = 259},
-	{description = "Numpad Four", data = 260},
-	{description = "Numpad Five", data = 261},
-	{description = "Numpad Six", data = 262},
-	{description = "Numpad Seven", data = 263},
-	{description = "Numpad Eight", data = 264},
-	{description = "Numpad Nine", data = 265},
-	{description = "Numpad Period", data = 266},
-	{description = "Numpad Divide", data = 267},
-	{description = "Numpad Multiply", data = 268},
-	{description = "Numpad Minus", data = 269},
-	{description = "Numpad Plus", data = 270},
---	{description = "Numpad Enter", data = 271},
---	{description = "Numpad Equals", data = 272},
-	{description = "Up", data = 273},
-	{description = "Down", data = 274},
-	{description = "Left", data = 276},
-	{description = "Right", data = 275},
-	{description = "Minus", data = 45},
-	{description = "Plus", data = 43},
-	{description = "LeftBracket", data = 91},
-	{description = "RightBracket", data = 93},
-	{description = "Semicolon", data = 59},
-	{description = "Quote", data = 39},
-	{description = "Comma", data = 44},
-	{description = "Period", data = 46},
-	{description = "Slash", data = 47},
-	{description = "PageUp", data = 280},
-	{description = "PageDown", data = 281},
-	{description = "Home", data = 278},
-	{description = "End", data = 279},
-	{description = "Insert", data = 277},
-	{description = "Delete", data = 127},
-	{description = "-- None --", data = 0}, -- Placing none here could help others to set up their keys
-	{description = "A", data = 97},
-	{description = "B", data = 98},
-	{description = "C", data = 99},
-	{description = "D", data = 100},
-	{description = "E", data = 101},
-	{description = "F", data = 102},
-	{description = "G", data = 103},
-	{description = "H", data = 104},
-	{description = "I", data = 105},
-	{description = "J", data = 106},
-	{description = "K", data = 107},
-	{description = "L", data = 108},
-	{description = "M", data = 109},
-	{description = "N", data = 110},
-	{description = "O", data = 111},
-	{description = "P", data = 112},
-	{description = "Q", data = 113},
-	{description = "R", data = 114},
-	{description = "S", data = 115},
-	{description = "T", data = 116},
-	{description = "U", data = 117},
-	{description = "V", data = 118},
-	{description = "W", data = 119},
-	{description = "X", data = 120},
-	{description = "Y", data = 121},
-	{description = "Z", data = 122},
-	{description = "F1", data = 282},
-	{description = "F2", data = 283},
-	{description = "F3", data = 284},
-	{description = "F4", data = 285},
-	{description = "F5", data = 286},
-	{description = "F6", data = 287},
-	{description = "F7", data = 288},
-	{description = "F8", data = 289},
-	{description = "F9", data = 290},
-	{description = "F10", data = 291},
-	{description = "F11", data = 292},
-	{description = "F12", data = 293},
-	{description = "-- None", data = -1}, -- To avoid forcing players to click all the way back to the beginning
+  { description = 'None --', data = -1 }, -- If players can't find the None option
+  { description = 'Numpad Zero', data = 256 },
+  { description = 'Numpad One', data = 257 },
+  { description = 'Numpad Two', data = 258 },
+  { description = 'Numpad Three', data = 259 },
+  { description = 'Numpad Four', data = 260 },
+  { description = 'Numpad Five', data = 261 },
+  { description = 'Numpad Six', data = 262 },
+  { description = 'Numpad Seven', data = 263 },
+  { description = 'Numpad Eight', data = 264 },
+  { description = 'Numpad Nine', data = 265 },
+  { description = 'Numpad Period', data = 266 },
+  { description = 'Numpad Divide', data = 267 },
+  { description = 'Numpad Multiply', data = 268 },
+  { description = 'Numpad Minus', data = 269 },
+  { description = 'Numpad Plus', data = 270 },
+  --	{description = "Numpad Enter", data = 271},
+  --	{description = "Numpad Equals", data = 272},
+  { description = 'Up', data = 273 },
+  { description = 'Down', data = 274 },
+  { description = 'Left', data = 276 },
+  { description = 'Right', data = 275 },
+  { description = 'Minus', data = 45 },
+  { description = 'Plus', data = 43 },
+  { description = 'LeftBracket', data = 91 },
+  { description = 'RightBracket', data = 93 },
+  { description = 'Semicolon', data = 59 },
+  { description = 'Quote', data = 39 },
+  { description = 'Comma', data = 44 },
+  { description = 'Period', data = 46 },
+  { description = 'Slash', data = 47 },
+  { description = 'PageUp', data = 280 },
+  { description = 'PageDown', data = 281 },
+  { description = 'Home', data = 278 },
+  { description = 'End', data = 279 },
+  { description = 'Insert', data = 277 },
+  { description = 'Delete', data = 127 },
+  { description = '-- None --', data = 0 }, -- Placing none here could help others to set up their keys
+  { description = 'A', data = 97 },
+  { description = 'B', data = 98 },
+  { description = 'C', data = 99 },
+  { description = 'D', data = 100 },
+  { description = 'E', data = 101 },
+  { description = 'F', data = 102 },
+  { description = 'G', data = 103 },
+  { description = 'H', data = 104 },
+  { description = 'I', data = 105 },
+  { description = 'J', data = 106 },
+  { description = 'K', data = 107 },
+  { description = 'L', data = 108 },
+  { description = 'M', data = 109 },
+  { description = 'N', data = 110 },
+  { description = 'O', data = 111 },
+  { description = 'P', data = 112 },
+  { description = 'Q', data = 113 },
+  { description = 'R', data = 114 },
+  { description = 'S', data = 115 },
+  { description = 'T', data = 116 },
+  { description = 'U', data = 117 },
+  { description = 'V', data = 118 },
+  { description = 'W', data = 119 },
+  { description = 'X', data = 120 },
+  { description = 'Y', data = 121 },
+  { description = 'Z', data = 122 },
+  { description = 'F1', data = 282 },
+  { description = 'F2', data = 283 },
+  { description = 'F3', data = 284 },
+  { description = 'F4', data = 285 },
+  { description = 'F5', data = 286 },
+  { description = 'F6', data = 287 },
+  { description = 'F7', data = 288 },
+  { description = 'F8', data = 289 },
+  { description = 'F9', data = 290 },
+  { description = 'F10', data = 291 },
+  { description = 'F11', data = 292 },
+  { description = 'F12', data = 293 },
+  { description = '-- None', data = -1 }, -- To avoid forcing players to click all the way back to the beginning
 }
 
-configuration_options =
-{
-    {
-        name = "buttonEnabled",
-        label = "Button Enabled",
-        options =
-        {
-            {description = "Yes", data = true},
-            {description = "No", data = false},
-
-        },
-        default = true,
+configuration_options = {
+  {
+    name = 'buttonEnabled',
+    label = 'Button Enabled',
+    options = {
+      { description = 'Yes', data = true },
+      { description = 'No', data = false },
     },
-    {
-        name = "keybind",
-        label = "Keybind",
-        options = Keys,
-        default = 0,
-    }
+    default = true,
+  },
+  {
+    name = 'keybind',
+    label = 'Keybind',
+    options = Keys,
+    default = 0,
+  },
 }
--- modmain.lua
+++ modmain.lua
@@ -1,115 +1,104 @@
-local ShadowToggleWidget = require "widgets/shadowtogglewidget"
-local shadowcreatures = {"crawlinghorror","terrorbeak","crawlingnightmare","nightmarebeak"}
+local ShadowToggleWidget = require('widgets/shadowtogglewidget')
+local shadowcreatures = { 'crawlinghorror', 'terrorbeak', 'crawlingnightmare', 'nightmarebeak' }
 
-local buttonEnabled = GetModConfigData("buttonEnabled")
+local buttonEnabled = GetModConfigData('buttonEnabled')
 
 Assets = {
-	Asset("ATLAS","images/shadowbuttonbackground.xml"),
-    Asset("IMAGE","images/shadowbuttonbackground.tex"),
-	Asset("ATLAS","images/shadowbuttonon.xml"),
-    Asset("IMAGE","images/shadowbuttonon.tex"),
-    Asset("ATLAS","images/shadowbuttonoff.xml"),
-    Asset("IMAGE","images/shadowbuttonoff.tex"),
+  Asset('ATLAS', 'images/shadowbuttonbackground.xml'),
+  Asset('IMAGE', 'images/shadowbuttonbackground.tex'),
+  Asset('ATLAS', 'images/shadowbuttonon.xml'),
+  Asset('IMAGE', 'images/shadowbuttonon.tex'),
+  Asset('ATLAS', 'images/shadowbuttonoff.xml'),
+  Asset('IMAGE', 'images/shadowbuttonoff.tex'),
 }
 
 local function CheckForSkeletonHat()
-    local head_item
-    if GLOBAL.EQUIPSLOTS.HEAD then
-        head_item = GLOBAL.ThePlayer.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HEAD)
-    else
-        head_item = GLOBAL.ThePlayer.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HAT)
-    end
-    if head_item and head_item.prefab == "skeletonhat" then
-        return true
-    else
-        return false
-    end
+  local head_item
+  if GLOBAL.EQUIPSLOTS.HEAD then
+    head_item = GLOBAL.ThePlayer.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HEAD)
+  else
+    head_item = GLOBAL.ThePlayer.replica.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.HAT)
+  end
+  if head_item and head_item.prefab == 'skeletonhat' then
+    return true
+  else
+    return false
+  end
 end
 
 --shadows constantly change transparency, so we have to overwrite some functions so they stay invisible when they should
-AddClassPostConstruct("components/transparentonsanity",function(self)
-    local function PushAlpha(self, alpha, most_alpha)
-        if GLOBAL.ThePlayer ~= nil and GLOBAL.ThePlayer.hasshadowsenabled == false then
-            self.inst.AnimState:OverrideMultColour(1, 1, 1, 0)
-            self.inst:Hide()
-        else
-            self.inst.AnimState:OverrideMultColour(1, 1, 1, alpha)
-            if self.inst.SoundEmitter ~= nil then
-                self.inst.SoundEmitter:OverrideVolumeMultiplier(alpha / most_alpha)
-            end
-            if self.onalphachangedfn ~= nil then
-                self.onalphachangedfn(self.inst, alpha, most_alpha)
-            end
-        end
+AddClassPostConstruct('components/transparentonsanity', function(self)
+  local function PushAlpha(self, alpha, most_alpha)
+    if GLOBAL.ThePlayer ~= nil and GLOBAL.ThePlayer.hasshadowsenabled == false then
+      self.inst.AnimState:OverrideMultColour(1, 1, 1, 0)
+      self.inst:Hide()
+    else
+      self.inst.AnimState:OverrideMultColour(1, 1, 1, alpha)
+      if self.inst.SoundEmitter ~= nil then self.inst.SoundEmitter:OverrideVolumeMultiplier(alpha / most_alpha) end
+      if self.onalphachangedfn ~= nil then self.onalphachangedfn(self.inst, alpha, most_alpha) end
     end
+  end
 
-    function self:DoUpdate(dt, force)
-        self.offset = self.offset + dt
-        self.target_alpha = self:CalcaulteTargetAlpha()
+  function self:DoUpdate(dt, force)
+    self.offset = self.offset + dt
+    self.target_alpha = self:CalcaulteTargetAlpha()
 
-        if force then
-            self.alpha = self.target_alpha
-            PushAlpha(self, self.alpha, self.most_alpha)
-        elseif self.alpha ~= self.target_alpha then
-            self.alpha = self.alpha > self.target_alpha and
-                math.max(self.target_alpha, self.alpha - dt) or
-                math.min(self.target_alpha, self.alpha + dt)
-            PushAlpha(self, self.alpha, self.most_alpha)
-        end
+    if force then
+      self.alpha = self.target_alpha
+      PushAlpha(self, self.alpha, self.most_alpha)
+    elseif self.alpha ~= self.target_alpha then
+      self.alpha = self.alpha > self.target_alpha and math.max(self.target_alpha, self.alpha - dt)
+        or math.min(self.target_alpha, self.alpha + dt)
+      PushAlpha(self, self.alpha, self.most_alpha)
     end
+  end
 end)
 
-AddClassPostConstruct("widgets/controls", function(self)
-    GLOBAL.ThePlayer.hasshadowsenabled = true
-    if buttonEnabled then
-        self.shadowtogglewidget = self:AddChild(ShadowToggleWidget(self.owner))
-        self.shadowtogglewidget:MoveToBack()
+AddClassPostConstruct('widgets/controls', function(self)
+  GLOBAL.ThePlayer.hasshadowsenabled = true
+  if buttonEnabled then
+    self.shadowtogglewidget = self:AddChild(ShadowToggleWidget(self.owner))
+    self.shadowtogglewidget:MoveToBack()
 
-        GLOBAL.ThePlayer:ListenForEvent("TurnOnShadows", function()
-            self.shadowtogglewidget.buttonOverlay:Show()
-        end)
-        GLOBAL.ThePlayer:ListenForEvent("TurnOffShadows", function() -- REQUIRE HELMET
-            self.shadowtogglewidget.buttonOverlay:Hide()
-        end)
-    end
-end)
-
-local function InGame()
-    return GLOBAL.ThePlayer and GLOBAL.ThePlayer.HUD and not GLOBAL.ThePlayer.HUD:HasInputFocus()
-end
-
-if GetModConfigData("keybind") and GetModConfigData("keybind") > 0 then -- viktor code :3
-	GLOBAL.TheInput:AddKeyDownHandler(GetModConfigData("keybind"),function()
-		if InGame() and GLOBAL.TheWorld then
-            GLOBAL.ThePlayer.hasshadowsenabled = not GLOBAL.ThePlayer.hasshadowsenabled
-            if GLOBAL.ThePlayer.hasshadowsenabled == true then
-                GLOBAL.ThePlayer:PushEvent("TurnOnShadows")
-            end
-            if GLOBAL.ThePlayer.hasshadowsenabled == false then
-                GLOBAL.ThePlayer:PushEvent("TurnOffShadows")
-            end
-			if GLOBAL.ThePlayer and GLOBAL.ThePlayer.components and GLOBAL.ThePlayer.components.talker then
-				GLOBAL.ThePlayer.components.talker:Say("BHT: Shadows "..(GLOBAL.ThePlayer.hasshadowsenabled and "Enabled" or "Disabled"))
-			end
-		end
-	end)
-end
-
-for i,prefab in ipairs(shadowcreatures) do
-    AddPrefabPostInit(prefab, function(inst)
-        inst:DoTaskInTime(0,function() --shadows might be initialized before the player
-            if GLOBAL.ThePlayer.hasshadowsenabled == false then
-                inst.AnimState:OverrideMultColour(1, 1, 1, 0)
-                inst:Hide()
-            end
-            GLOBAL.ThePlayer:ListenForEvent("TurnOnShadows", function()
-                inst.AnimState:OverrideMultColour(1, 1, 1, 0.4)
-                inst:Show()
-            end)
-            GLOBAL.ThePlayer:ListenForEvent("TurnOffShadows", function()
-                inst.AnimState:OverrideMultColour(1, 1, 1, 0)
-                inst:Hide()
-            end)
-        end)
+    GLOBAL.ThePlayer:ListenForEvent('TurnOnShadows', function() self.shadowtogglewidget.buttonOverlay:Show() end)
+    GLOBAL.ThePlayer:ListenForEvent('TurnOffShadows', function() -- REQUIRE HELMET
+      self.shadowtogglewidget.buttonOverlay:Hide()
     end)
+  end
+end)
+
+local function InGame() return GLOBAL.ThePlayer and GLOBAL.ThePlayer.HUD and not GLOBAL.ThePlayer.HUD:HasInputFocus() end
+
+if GetModConfigData('keybind') and GetModConfigData('keybind') > 0 then -- viktor code :3
+  GLOBAL.TheInput:AddKeyDownHandler(GetModConfigData('keybind'), function()
+    if InGame() and GLOBAL.TheWorld then
+      GLOBAL.ThePlayer.hasshadowsenabled = not GLOBAL.ThePlayer.hasshadowsenabled
+      if GLOBAL.ThePlayer.hasshadowsenabled == true then GLOBAL.ThePlayer:PushEvent('TurnOnShadows') end
+      if GLOBAL.ThePlayer.hasshadowsenabled == false then GLOBAL.ThePlayer:PushEvent('TurnOffShadows') end
+      if GLOBAL.ThePlayer and GLOBAL.ThePlayer.components and GLOBAL.ThePlayer.components.talker then
+        GLOBAL.ThePlayer.components.talker:Say(
+          'BHT: Shadows ' .. (GLOBAL.ThePlayer.hasshadowsenabled and 'Enabled' or 'Disabled')
+        )
+      end
+    end
+  end)
+end
+
+for i, prefab in ipairs(shadowcreatures) do
+  AddPrefabPostInit(prefab, function(inst)
+    inst:DoTaskInTime(0, function() --shadows might be initialized before the player
+      if GLOBAL.ThePlayer.hasshadowsenabled == false then
+        inst.AnimState:OverrideMultColour(1, 1, 1, 0)
+        inst:Hide()
+      end
+      GLOBAL.ThePlayer:ListenForEvent('TurnOnShadows', function()
+        inst.AnimState:OverrideMultColour(1, 1, 1, 0.4)
+        inst:Show()
+      end)
+      GLOBAL.ThePlayer:ListenForEvent('TurnOffShadows', function()
+        inst.AnimState:OverrideMultColour(1, 1, 1, 0)
+        inst:Hide()
+      end)
+    end)
+  end)
 end
